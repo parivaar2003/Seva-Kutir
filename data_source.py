0738ccbc08f6d6a2834d6d295d15928015b45160
ganesh-raj-paramasivam <ganesh.param@tigeranalytics.com>
2025-06-30 17:20:20 +0530

    Update data_source.py

--- data_source.py
+++ data_source.py
@@ -50,7 +50,7 @@ class data:
         
         self.sheet = pd.DataFrame(data_rows, columns=unique_headers)
         self.sheet.columns = self.sheet.columns.str.split('-').str[0].str.strip()
-        self.rename_columns()
+        #self.rename_columns()
         self.convert_column_types()
         self.precompute_periods()
 
