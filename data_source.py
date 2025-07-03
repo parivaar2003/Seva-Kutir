985b1a78d56b64052744b5536b1979f8620486c4
ganesh-raj-paramasivam <ganesh.param@tigeranalytics.com>
2025-07-03 11:13:37 +0530

    Update data_source.py

--- data_source.py
+++ data_source.py
@@ -51,6 +51,7 @@ class data:
         final_unique_headers = self.make_columns_unique(cleaned_headers)
         
         self.sheet = pd.DataFrame(data_rows, columns=final_unique_headers)
+        self.sheet['Date']=self.sheet['Timestamp'].dt.date
 
         kutir_name_cols = [col for col in self.sheet.columns if col.startswith('Kutir Name')]
         if len(kutir_name_cols) > 1:
